0950f920be47268a35fcf807c5f1afb65747a3c6
Tauan Dos Santos <tauansant2608@gmail.com>
2023-11-28 20:33:03 -0300

    adicionando exercicio que introduz o conceito de função

---
+++ exercicio17.py
@@ -0,0 +1,11 @@
+# Desafio1
+def gerar_nome_completo(nome,sobrenome):
+    print(f'Boas vindas {nome} {sobrenome}')
+
+
+gerar_nome_completo('Tauan','Ferreira')
+
+# Desafio2
+def calcular_valores(preço,quantidade=1):
+    print(preço*quantidade)
+calcular_valores(8,2)
