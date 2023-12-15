926c29fb60abae5441eb7e2ac9bc73dc67716976
Tauan Dos Santos <tauansant2608@gmail.com>
2023-12-14 21:42:44 -0300

    Projeto-calculadora-simples

---
+++ calculadora_simples.py
@@ -0,0 +1,30 @@
+calculadora = input('Qual operação deseja realizar? Soma[s],Subtração[su],Multiplicação[m] e divisão[d]')
+if calculadora == 's':
+    numero1 = int(input('Número1: '))
+    numero2 = int(input('Número2: '))
+    resultado = numero1 + numero2
+    print(resultado)
+elif calculadora == 'su':
+    numero1 = int(input('Número1: '))
+    numero2 = int(input('Número2: '))
+    resultado = numero1 - numero2
+    print(resultado)
+elif calculadora == 'm':
+    numero1 = int(input('Número1: '))
+    numero2 = int(input('Número2: '))
+    resultado = numero1 * numero2
+    print(resultado)
+elif calculadora == 'd':
+    numero1 = int(input('Número1: '))
+    numero2 = int(input('Número2: '))
+    resultado = numero1 / numero2
+    print(int(resultado))
+    
+       
+                
+        
+       
+        
+
+
+
